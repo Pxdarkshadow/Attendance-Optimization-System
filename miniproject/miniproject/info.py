edf666c650f89fc569b7dd3819796006e33c206c
PrMayekar <pranavmayekar07@gmail.com>
2025-08-15 20:23:37 +0530

    Removed the app password for security purpose

--- miniproject/miniproject/info.py
+++ miniproject/miniproject/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mayekarpranav05@gmail.com'
-EMAIL_HOST_PASSWORD = 'bvks ginv caji fhjn'
+EMAIL_HOST_USER = 'mayekarpranav05@gmail.com' # Use your own email
+EMAIL_HOST_PASSWORD = 'bvks ginv caji fhjn' # Use your own app password
 EMAIL_PORT = 587
