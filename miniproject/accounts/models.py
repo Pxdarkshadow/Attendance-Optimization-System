c524a88e14ffa4a409872b2d2330f45f20a7c09d
PrMayekar <pranavmayekar07@gmail.com>
2025-09-17 23:56:44 +0530

    User can enter academic calendar

--- miniproject/accounts/models.py
+++ miniproject/accounts/models.py
@@ -1,24 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Timetable(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="timetables")
-    slot = models.AutoField(primary_key=True)
-    time = models.TimeField()
-    monday = models.CharField(max_length=20, blank=True, null=True)
-    tuesday = models.CharField(max_length=20, blank=True, null=True)
-    wednesday = models.CharField(max_length=20, blank=True, null=True)
-    thursday = models.CharField(max_length=20, blank=True, null=True)
-    friday = models.CharField(max_length=20, blank=True, null=True)
+# class Timetable(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="timetables")
+#     slot = models.AutoField(primary_key=True)
+#     time = models.TimeField()
+#     monday = models.CharField(max_length=20, blank=True, null=True)
+#     tuesday = models.CharField(max_length=20, blank=True, null=True)
+#     wednesday = models.CharField(max_length=20, blank=True, null=True)
+#     thursday = models.CharField(max_length=20, blank=True, null=True)
+#     friday = models.CharField(max_length=20, blank=True, null=True)
 
-    class Meta:
-        db_table = "timetable"
+#     class Meta:
+#         db_table = "timetable"
 
 class AcademicCalendar(models.Model):
-    acdid = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="AcademicCalendars")
     date = models.DateField()
     context = models.CharField(max_length=50)
 
     class Meta:
         db_table = "AcademicCalendar"
+
+
+class Subject(models.Model):
+    subjectName = models.CharField(max_length=30, unique=True, null=False)
+    facultyName = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subjects")
+
+    def __str__(self):
+        return self.subjectName
+    
+    class Meta:
+        db_table = "Subject"
+
+class TimeTable(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="timetables")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="timetables")
+    day = models.CharField(max_length=10,null=False)
+    startTime = models.TimeField(null=False)
+    endTime = models.TimeField(null=False)
+
+    def __str__(self):
+        return f"{self.subject.subjectName} on {self.day} at {self.startTime}" 
+    
+    class Meta:
+        db_table = "TimeTable"
--- miniproject/accounts/urls.py
+++ miniproject/accounts/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
     path('signin',views.signin,name="signin"),
     path('signout',views.signout,name="signout"),
     path('activate/<uidb64>/<token>',views.activate,name="activate"),
-    path('dashboard',views.upload_timetable,name="dashboard"),
+    # path('dashboard',views.upload_timetable,name="dashboard"),
     path('dashboard2',views.upload_acdcalendar,name="dashboard2"),
     path('insights',views.userprofile,name="insights"),
+    path('homepage',views.homepage,name="homepage"),
+    path('db1',views.userinput,name="db1"),
+    path('db2',views.upload_timetable,name="db2"),
+    path('db3',views.upload_academiccalendar,name="db3"),
 ]
